75d9c91c13c213d85f59c2ad92f995e647a647b4
sesan-7 <senabadhy.nrc07@gmail.com>
2026-02-26 22:59:55 +0530

    production update

--- web/privacy.js
+++ web/privacy.js
@@ -4,12 +4,6 @@ import { DeliveryMethod } from "@shopify/shopify-api";
  * @type {{[key: string]: import("@shopify/shopify-api").WebhookHandler}}
  */
 export default {
-  /**
-   * Customers can request their data from a store owner. When this happens,
-   * Shopify invokes this privacy webhook.
-   *
-   * https://shopify.dev/docs/apps/webhooks/configuration/mandatory-webhooks#customers-data_request
-   */
   CUSTOMERS_DATA_REQUEST: {
     deliveryMethod: DeliveryMethod.Http,
     callbackUrl: "/api/webhooks",
@@ -38,30 +32,13 @@ export default {
 
   /**
    * Store owners can request that data is deleted on behalf of a customer. When
-   * this happens, Shopify invokes this privacy webhook.
-   *
-   * https://shopify.dev/docs/apps/webhooks/configuration/mandatory-webhooks#customers-redact
+
    */
   CUSTOMERS_REDACT: {
     deliveryMethod: DeliveryMethod.Http,
     callbackUrl: "/api/webhooks",
     callback: async (topic, shop, body, webhookId) => {
       const payload = JSON.parse(body);
-      // Payload has the following shape:
-      // {
-      //   "shop_id": 954889,
-      //   "shop_domain": "{shop}.myshopify.com",
-      //   "customer": {
-      //     "id": 191167,
-      //     "email": "john@example.com",
-      //     "phone": "555-625-1199"
-      //   },
-      //   "orders_to_redact": [
-      //     299938,
-      //     280263,
-      //     220458
-      //   ]
-      // }
     },
   },
 
@@ -76,11 +53,6 @@ export default {
     callbackUrl: "/api/webhooks",
     callback: async (topic, shop, body, webhookId) => {
       const payload = JSON.parse(body);
-      // Payload has the following shape:
-      // {
-      //   "shop_id": 954889,
-      //   "shop_domain": "{shop}.myshopify.com"
-      // }
     },
   },
 };
